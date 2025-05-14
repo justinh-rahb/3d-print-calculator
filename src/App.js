d752b63b1ca3086cf627115a4b94a49f4f3f7d6c
Justin Hayes <justinh@rahb.ca>
2025-05-14 13:09:03 -0400

    Add GH link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 import PrintQuoteCalculator from './components/PrintQuoteCalculator';
+import { Github } from 'lucide-react';
 
 function App() {
   return (
     <div className="App">
+      <a 
+        href="https://github.com/justinh-rahb/3d-print-calculator" 
+        target="_blank"
+        rel="noopener noreferrer"
+        className="github-link"
+      >
+        <Github size={24} />
+        <span>View on GitHub</span>
+      </a>
       <PrintQuoteCalculator />
     </div>
   );
